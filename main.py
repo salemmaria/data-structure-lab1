579c0f3c4d6e10cf41296e08d6016f7a1c777d4f
Maria Salem <salemmaria2005@gmail.com>
2024-08-22 11:47:50 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# num1 = input("Enter first number: ")
+# num2 = input("Enter second number: ")
+# if num1 < num2:
+#    print(f"{num1} is less than {num2}")
+# elif num1 > num2:
+#    print(f"{num1} is greater than {num2}")
+# else:
+#    print(f"{num1} is equals to {num2}")
+
+classmate1 = input("Enter classmate name 1: ")
+classmate2 = input("Enter classmate name 2: ")
+classmate3 = input("Enter classmate name 3: ")
+print(f"Your classmates are: {classmate1}, {classmate2}, & {classmate3}.")
